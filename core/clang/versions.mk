672ef43c06c727ffee32723c8097a7a610fd9236
Inventor1938 <ivan.kolev@mail.bg>
2017-07-12 23:13:53 +0300

    update host clang version

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,9 +1,9 @@
 ## Clang/LLVM release versions.
-LLVM_PREBUILTS_VERSION := clang-3859424
+LLVM_PREBUILTS_VERSION := clang-4053586
 LLVM_PREBUILTS_BASE := prebuilts/clang/host
 
 ## RenderScript-specific tools
-RS_LLVM_PREBUILTS_VERSION := clang-3859424
+RS_LLVM_PREBUILTS_VERSION := clang-4053586
 RS_LLVM_PREBUILTS_BASE := prebuilts/clang/host
 
 ## SDCLANG
